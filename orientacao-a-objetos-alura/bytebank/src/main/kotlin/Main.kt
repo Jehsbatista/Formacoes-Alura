ffe2b44943ec4d446be534128cf7cc96d14b876c
jessica.batista <jessica.batista@g.globo>
2023-02-07 21:47:20 -0300

    criando projeto bytebank

---
+++ orientacao-a-objetos-alura/bytebank/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    println("Hello World!")
+    println("Hello World!")
+    println("Bem vindo ao Bytebank")
+    val titular: String = "Alex"
+    val numeroConta: Int = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("titular $titular")
+    println("número da conta $numeroConta")
+    println("saldo da conta $saldo")
+}
