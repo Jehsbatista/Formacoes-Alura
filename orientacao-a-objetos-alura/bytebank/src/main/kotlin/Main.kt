33582000940d16e6b2697ab7d88fdc17d80e4432
jessica.batista <jessica.batista@g.globo>
2023-02-09 19:31:18 -0300

    criando novos objetos para diferenciar (cópia de referência)

--- orientacao-a-objetos-alura/bytebank/src/main/kotlin/Main.kt
+++ orientacao-a-objetos-alura/bytebank/src/main/kotlin/Main.kt
@@ -22,6 +22,21 @@ fun main() {
     //val saldo = 0.0
     //testaLacos()
     //testacondicoes(saldo)
+
+    //criando novos objetos, uma cópia
+
+    val contaJoao1 = Conta()
+    contaJoao1.titular = "João"
+    var contaMaria1 = Conta()
+    contaMaria1.titular = "Maria"
+
+    //desta forma esta pegando a referência do objeto contajoao
+
+    val contaJoao2 = Conta()
+    contaJoao2.titular = "João"
+    val contaMaria2 = contaJoao1
+    contaMaria2.titular = "Maria"
+
 }
 
 class Conta {
