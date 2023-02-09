298bf279507ce15574197d924c3cc7c268f3669d
jessica.batista <jessica.batista@g.globo>
2023-02-08 21:21:05 -0300

    criando Classe Conta, objetos e impressão dos seus respectivos valores

--- orientacao-a-objetos-alura/bytebank/src/main/kotlin/Main.kt
+++ orientacao-a-objetos-alura/bytebank/src/main/kotlin/Main.kt
@@ -1,8 +1,33 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
+    val contaAlex = Conta()
+    contaAlex.titular = "Alex"
+    contaAlex.numero = 1000
+    contaAlex.saldo = 200.0
+
+    val contaFran = Conta()
+    contaFran.titular = "Fran"
+    contaFran.numero = 1001
+    contaFran.saldo = 300.0
+
+    println(contaAlex.titular)
+    println(contaAlex.numero)
+    println(contaAlex.saldo)
+
+    println(contaFran.titular)
+    println(contaFran.numero)
+    println(contaFran.saldo)
+
+    //val saldo = 0.0
     //testaLacos()
     //testacondicoes(saldo)
+}
+
+class Conta {
+    var titular = ""
+    var numero = 0
+    var saldo = 0.0
 }
 
 fun testaLacos() {
