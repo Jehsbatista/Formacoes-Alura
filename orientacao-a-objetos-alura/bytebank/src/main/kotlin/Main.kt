7207ae09542163c6d26232496b3ef7cb584aafd0
jessica.batista <jessica.batista@g.globo>
2023-02-14 21:51:33 -0300

    criando uma nova classe Funcionario, adicionando a ela um método de bonificação e e gerando uma nova instancia da classe funcionario

--- orientacao-a-objetos-alura/bytebank/src/main/kotlin/Main.kt
+++ orientacao-a-objetos-alura/bytebank/src/main/kotlin/Main.kt
@@ -1,6 +1,15 @@
 fun main() {
     println("Bem vindo ao Bytebank")
-    testaLacos()
+
+    val alex = Funcionario(
+        nome = "Alex",
+        cpf = "111.111.111-11",
+        salario = 1000.0
+    )
+    println("nome: ${alex.nome}")
+    println("cpf: ${alex.cpf}")
+    println("salário: ${alex.salario}")
+    println("Bonificação ${alex.bonificacao()}")
 }
 
 
