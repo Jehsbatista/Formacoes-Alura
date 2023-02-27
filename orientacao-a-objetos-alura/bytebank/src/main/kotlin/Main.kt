2dd4bbbc95da5dc13dd027ff7f30e98ced545f4c
jessica.batista <jessica.batista@g.globo>
2023-02-27 19:33:27 -0300

    criando método testacontasdiferentes para organização do código

--- orientacao-a-objetos-alura/bytebank/src/main/kotlin/Main.kt
+++ orientacao-a-objetos-alura/bytebank/src/main/kotlin/Main.kt
@@ -1,32 +1,6 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    val contaCorrente = ContaCorrente(
-        titular = "Alex",
-        numero = 1000
-    )
-
-    val contaPoupanca = ContaPoupanca(
-        titular = "Fran",
-        numero = 1001
-    )
-
-    contaCorrente.deposita(1000.0)
-    contaPoupanca.deposita(1000.0)
-
-    println("Saldo corrente: ${contaCorrente.saldo}")
-    println("Saldo poupança: ${contaPoupanca.saldo}")
-
-    contaCorrente.saca(100.0)
-    contaPoupanca.saca(100.0)
-
-    println("Saldo após saque corrente: ${contaCorrente.saldo}")
-    println("Saldo após saque poupança: ${contaPoupanca.saldo}")
-
-    contaCorrente.transfere(100.0, contaPoupanca)
-
-    println("Saldo corrente após tranferir para poupança: ${contaCorrente.saldo}")
-    println("Saldo poupança após receber transferência: ${contaPoupanca.saldo}")
 }
 
 
