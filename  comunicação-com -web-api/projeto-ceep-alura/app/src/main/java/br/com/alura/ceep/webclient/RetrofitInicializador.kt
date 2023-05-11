89b5e78a8bafb8963899aa6d3d14c3311e9a8845
jessica.batista <jessica.batista@g.globo>
2023-05-11 20:59:23 -0300

    Configurando o Retrofit parte 2: criado uma instância do serviço da API chamado "NotaService", através dessa instância de "NotaService", é possível chamar os métodos definidos na interface

---  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
+++  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
@@ -1,10 +1,14 @@
 package br.com.alura.ceep.webclient
 
+import br.com.alura.ceep.webclient.services.NotaService
 import retrofit2.Retrofit
+import retrofit2.create
 
 class RetrofitInicializador {
 
     private val retrofit = Retrofit.Builder()
         .baseUrl("http://localhost:8080/")
         .build()
+
+    val notaServive = retrofit.create(NotaService::class.java)
 }
