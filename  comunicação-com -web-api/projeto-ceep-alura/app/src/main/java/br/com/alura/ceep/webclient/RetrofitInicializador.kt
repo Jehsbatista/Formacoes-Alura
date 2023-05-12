0af9f92aff223ca8070e7073575ab6df82a5922c
jessica.batista <jessica.batista@g.globo>
2023-05-11 21:19:56 -0300

    Adicionando um conversor (Mochi) - Essa configuração é importante para garantir que o Retrofit possa lidar corretamente com a conversão de dados entre o formato JSON e objetos Kotlin

---  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
+++  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
@@ -2,12 +2,14 @@ package br.com.alura.ceep.webclient
 
 import br.com.alura.ceep.webclient.services.NotaService
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 
 class RetrofitInicializador {
 
     private val retrofit = Retrofit.Builder()
         .baseUrl("http://localhost:8080/")
+        .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
     val notaServive = retrofit.create(NotaService::class.java)
