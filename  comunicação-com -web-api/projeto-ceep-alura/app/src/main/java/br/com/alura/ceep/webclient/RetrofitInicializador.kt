872830810b3e584eb6f59dc187418ca220b087aa
jessica.batista <jessica.batista@g.globo>
2023-05-11 21:55:31 -0300

    Solucionando falha de conexão com o endereço localhost.

---  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
+++  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/RetrofitInicializador.kt
@@ -8,7 +8,7 @@ import retrofit2.create
 class RetrofitInicializador {
 
     private val retrofit = Retrofit.Builder()
-        .baseUrl("http://localhost:8080/")
+        .baseUrl("http://192.168.1.5:8080/")
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
 
