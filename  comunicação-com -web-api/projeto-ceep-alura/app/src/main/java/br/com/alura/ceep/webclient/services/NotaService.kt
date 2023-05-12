603ebc44e2ed1edf1d98982fc26c179802228702
jessica.batista <jessica.batista@g.globo>
2023-05-11 22:16:36 -0300

    Buscando notas da API, criando o modelo de nota compatível com o modelo da API.

---  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/services/NotaService.kt
+++  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/services/NotaService.kt
@@ -1,10 +1,10 @@
 package br.com.alura.ceep.webclient.services
 
-import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.webclient.model.NotaResposta
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface NotaService {
     @GET("notas")
-    fun buscarTodas(): Call<List<Nota>>
+    fun buscarTodas(): Call<List<NotaResposta>>
 }
