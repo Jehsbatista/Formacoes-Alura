02f332e9801884c6e35c31f7d86eea805867c25f
jessica.batista <jessica.batista@g.globo>
2023-05-30 21:18:03 -0300

    tratando o código caso haja Exceptions

---  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
+++  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/webclient/NotaWebClient.kt
@@ -1,12 +1,22 @@
 package br.com.alura.ceep.webclient
 
+import android.util.Log
 import br.com.alura.ceep.model.Nota
+import br.com.alura.ceep.webclient.services.NotaService
+
+private const val TAG = "webclient"
+private val notaService: NotaService = RetrofitInicializador().notaServive
 
 class NotaWebClient {
-    suspend fun buscaTodas(): List<Nota> {
-        val notasResposta = RetrofitInicializador().notaServive.buscaTodas()
-        return notasResposta.map { notaResposta ->
-            notaResposta.nota
+    suspend fun buscaTodas(): List<Nota>? {
+        return try {
+            val notasResposta = notaService.buscaTodas()
+            return notasResposta.map { notaResposta ->
+                notaResposta.nota
+            }
+        } catch (e: Exception) {
+            Log.e(TAG, "buscatodas")
+            null
         }
     }
 }
