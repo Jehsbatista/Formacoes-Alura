78a8a604a3b670059fdb428f9be3240478070fe5
jessica.batista <jessica.batista@g.globo>
2023-06-27 21:39:16 -0300

    Criando flags de sincronização

---  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/migrations/Migrations.kt
+++  comunicação-com -web-api/projeto-ceep-alura/app/src/main/java/br/com/alura/ceep/migrations/Migrations.kt
@@ -46,5 +46,9 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
         //renomear tabela nova com o nome da tabela atual
         database.execSQL("ALTER TABLE $tabelaNova RENAME TO $tabelaAtual")
     }
-
+}
+val MIGRATION_2_3 =  object : Migration(2,3) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE Nota ADD sincronizada INTEGER NOT NULL DEFAULT 0")
+    }
 }
