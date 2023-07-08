c4dab6fccee1e7cc818eb42f23210fe69c1a7a27
jessica.batista <jessica.batista@g.globo>
2023-07-08 11:59:01 -0300

    Copiando collections

--- collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
+++ collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 fun main() {
     val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
     banco.salva("alex")
+    println(nomesSalvos)
     println(BancoDeNomes().nomes)
 }
 
 class BancoDeNomes {
-    val nomes: Collection<String> get() = dados
+    val nomes: Collection<String> get() = dados.toList()
 
     fun salva(nome: String) {
         dados.add(nome)
