4168f74ea46d554d4df441a1832621335dc4000b
jessica.batista <jessica.batista@g.globo>
2023-07-11 21:05:38 -0300

    Organizando o código

--- collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
+++ collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
@@ -1,30 +1,3 @@
 fun main() {
-    val banco = BancoDeNomes()
-    val nomesSalvos: Collection<String> = banco.nomes
-    banco.salva("alex")
-    println(nomesSalvos)
-    println(BancoDeNomes().nomes)
-}
-
-class BancoDeNomes {
-    val nomes: Collection<String> get() = dados.toList()
-
-    fun salva(nome: String) {
-        dados.add(nome)
-    }
-
-    companion object {
-        private val dados = mutableListOf<String>()
-    }
-}
-
-fun testaColecao() {
-    val nomes: Collection<String> = mutableListOf("Jéssica", "Roberto", "Carlos", "Ana", "Maria")
-    println(nomes)
-
-    for (nome in nomes) {
-        println(nome)
-    }
-    println("Tem o nome Carlos na lista? ${nomes.contains("Carlos")}")
-    println("Quantos nomes possui essa lista? ${nomes.size}")
+    testaCopia()
 }
