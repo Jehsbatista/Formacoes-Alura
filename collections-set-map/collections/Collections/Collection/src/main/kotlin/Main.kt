f3bed0929bdfb8dafd35b92a95507d39256e624e
jessica.batista <jessica.batista@g.globo>
2023-07-17 18:54:18 -0300

    Utilizando o Set para não haver repetição de elementos

--- collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
+++ collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 fun main() {
     val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin: List<String> = listOf("Alex", "Paulo", "Maria")
-    val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
-
-    println(assistiramAmbos.distinct())
+    val assistiramCursoKotlin: Set<String> = setOf("Alex", "Paulo", "Maria")
+    //val assistiramAmbos: Set<String> = assistiramCursoAndroid + assistiramCursoKotlin
+    val assistiramAmbos = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana") //não repete o elemento
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
 }
