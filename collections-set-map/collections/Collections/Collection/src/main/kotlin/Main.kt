b2acd1160ddca66de8c5a7403d20c8a7b37f7451
jessica.batista <jessica.batista@g.globo>
2023-07-18 18:54:15 -0300

    Utilizando o map

--- collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
+++ collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
 fun main() {
-    testaSet()
+    val pedidosA = mapOf(
+        Pair(1, 20.0), Pair(2, 34.0)
+    ) //Pair armazena valores para o mapa
+
+    val pedidosB = mapOf(
+        1 to 20.0,
+        2 to 50.0
+    ) //utilizando infix "to" que faz o mesmo efeito do Pair. Não é recomendado para vários valores por perda de performance
+
+    println(pedidosB)
+
+    val pedidos = pedidosB[1]  //acessando valor por chave com tratamento caso seja nulo
+    pedidos?.let {
+        println("pedido $it")
+    }
+
+    for (pedido: Map.Entry<Int, Double> in pedidosB){
+        println("número do pedido: ${pedido.key}")
+        println("valor do pedido: ${pedido.value}")
+    }
 }
