e4632104459d277e4ac70b8946c6867fab30a6b9
jessica.batista <jessica.batista@g.globo>
2023-07-18 20:37:30 -0300

    organizando o código em um método e testando o getValue, onde ele retorna uma exception caso o valor não exista e o get retornando um null

--- collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
+++ collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
@@ -1,22 +1,22 @@
 fun main() {
-    val pedidosA = mapOf(
-        Pair(1, 20.0),
-        Pair(2, 34.0)
-    ) //Pair armazena valores para o mapa
 
-    val pedidosB: MutableMap<Int, Double> = mutableMapOf(
+    val pedidosB: MutableMap<Int, Double?> = mutableMapOf(
         1 to 20.0,
         2 to 50.0
     ) //utilizando infix "to" que faz o mesmo efeito do Pair. Não é recomendado para vários valores por perda de performance
 
-    println(pedidosB)
+    val valorPedido = pedidosB.getValue(3)//utilizando o get será retornado o null, com o getValue terá uma exception
+    println(valorPedido)
+}
 
+private fun testaMap(pedidosB: MutableMap<Int, Double>) {
+    println(pedidosB)
     val pedidos = pedidosB[1]  //acessando valor por chave com tratamento caso seja nulo
     pedidos?.let {
         println("pedido $it")
     }
 
-    for (pedido: Map.Entry<Int, Double> in pedidosB){
+    for (pedido: Map.Entry<Int, Double> in pedidosB) {
         println("número do pedido: ${pedido.key}")
         println("valor do pedido: ${pedido.value}")
     }
