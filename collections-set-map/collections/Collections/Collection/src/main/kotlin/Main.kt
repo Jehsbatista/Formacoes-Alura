ad166c6f68e5cc15812bbea3ffd2036f4ea1deba
jessica.batista <jessica.batista@g.globo>
2023-07-18 19:59:58 -0300

    comportamentos de escrita utilizando o map

--- collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
+++ collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
@@ -1,9 +1,10 @@
 fun main() {
     val pedidosA = mapOf(
-        Pair(1, 20.0), Pair(2, 34.0)
+        Pair(1, 20.0),
+        Pair(2, 34.0)
     ) //Pair armazena valores para o mapa
 
-    val pedidosB = mapOf(
+    val pedidosB: MutableMap<Int, Double> = mutableMapOf(
         1 to 20.0,
         2 to 50.0
     ) //utilizando infix "to" que faz o mesmo efeito do Pair. Não é recomendado para vários valores por perda de performance
@@ -19,4 +20,19 @@ fun main() {
         println("número do pedido: ${pedido.key}")
         println("valor do pedido: ${pedido.value}")
     }
+
+    //comportamentos do map
+    pedidosB[3] = 60.0  //insere elemento
+    println(pedidosB)
+
+    pedidosB[1] = 10.0  //atualiza o elemento
+    println(pedidosB)
+
+    pedidosB.putIfAbsent(4, 100.0) //insere novo valor, porém só atualiza o valor caso não tenha o mesmo
+    println(pedidosB)
+    pedidosB.putIfAbsent(4, 200.0)
+    println(pedidosB)
+
+    pedidosB.remove(4) //remove o elemento
+    println(pedidosB)
 }
