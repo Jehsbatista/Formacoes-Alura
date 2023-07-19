4441d115846aa1e699837cce80c890e0ad443576
jessica.batista <jessica.batista@g.globo>
2023-07-18 21:09:58 -0300

    leitura de filtros específicos de map

--- collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
+++ collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
@@ -1,12 +1,21 @@
 fun main() {
 
-    val pedidosB: MutableMap<Int, Double?> = mutableMapOf(
+    val pedidosB: MutableMap<Int, Double> = mutableMapOf(
         1 to 20.0,
-        2 to 50.0
+        2 to 50.0,
+        3 to 200.0
     ) //utilizando infix "to" que faz o mesmo efeito do Pair. Não é recomendado para vários valores por perda de performance
 
-    val valorPedido = pedidosB.getValue(3)//utilizando o get será retornado o null, com o getValue terá uma exception
-    println(valorPedido)
+    println(pedidosB.getOrElse(5) {//caso não encontre a chave será retornado outro valor desejado
+        "não tem o pedido"
+    })
+
+    println(pedidosB.keys)//retorna todas as chaves
+    println(pedidosB.values)//retorna todos os valores
+
+    println( pedidosB.filter { (numero: Int, valor: Double?) -> //faz um filtro dos valores desejados
+        numero > 2 && valor > 20.0
+    })
 }
 
 private fun testaMap(pedidosB: MutableMap<Int, Double>) {
