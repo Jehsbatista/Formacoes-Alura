3b50cf3319a4bf1ab558f6ee70713e080a01557d
jessica.batista <jessica.batista@g.globo>
2023-07-25 22:29:10 -0300

    Utilizando a associação

--- collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
+++ collections-set-map/collections/Collections/Collection/src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
+    val pedidos = listOf(
+        Pedido(1, 20.0),
+        Pedido(2, 60.0),
+        Pedido(3, 30.0),
+        Pedido(4, 70.0)
+    )
+    println(pedidos)
+    val pedidosMapeados: Map<Int, Pedido> = pedidos.associateBy { pedido -> pedido.numero }
+    println(pedidosMapeados)
+    println(pedidosMapeados[1])
 
+    val pedidosFreteGratis: Map<Pedido, Boolean> = pedidos.associateWith { pedido ->
+        pedido.valor > 50.0
+    }
+    println(pedidosFreteGratis)
+    println(pedidosFreteGratis[Pedido(numero = 1, valor = 20.0)])
 }
+
+data class Pedido(val numero: Int, val valor: Double)
